47cbf83f02f2c05a1b05f839dea983921182fcbc
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-04 10:30:41 -0300

    component: transactions: create transaction context
    Creates transaction `Context` to be able to use
    our list of transactions in other components, for example:
    
    - Summary (to calculate transaction summary)
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,7 +1,12 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
+import { useContext } from "react";
+import { TransactionContext } from "../../contexts/TransactionsContext";
 
 export function Summary() {
+
+  const { transactions } = useContext(TransactionContext)
+
   return(
     <SummaryContainer>
       <SummaryCard>
