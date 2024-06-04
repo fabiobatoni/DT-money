c529ebaf92c4007ed799567b860d93d2d0d78e18
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-04 11:04:39 -0300

    component: hooks: create hook for the summary
    Abstracts summary logic to a separate hook
    called by convention `useSummary`
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,31 +1,12 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { SummaryCard, SummaryContainer } from "./styles";
-import { useContext } from "react";
-import { TransactionContext } from "../../contexts/TransactionsContext";
 import { priceFormatter } from "../../utils/formatter";
+import { useSummary } from "../../hooks/useSummary";
 
 export function Summary() {
 
-  const { transactions } = useContext(TransactionContext)
+  const summary = useSummary();
 
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if(transaction.type === 'income'){
-        acc.income += transaction.price
-        acc.total += transaction.price
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    }, 
-    {
-      income: 0,
-      outcome: 0,
-      total: 0 
-    }
-    )
   return(
     <SummaryContainer>
       <SummaryCard>
