5e6a8ebb28d5d965beb69fa443484c4cc248bb5c
fabiobatoni <fabio_batoni@hotmail.com>
2024-06-05 15:30:54 -0300

    component: context: performance
    Apply the `useCallback`
    
    Signed-off-by: fabiobatoni <fabio_batoni@hotmail.com>

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useState, useCallback } from "react";
 import { api } from "../lib/axios";
 import { createContext } from "use-context-selector";
 
@@ -34,20 +34,22 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
 
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  async function fetchTransactions(query?: string) {
+    const fetchTransactions = useCallback(async (query?: string) => {
 
-    const response = await api.get('transactions', {
-      params: {
-        _sort: 'createdAt',
-        _order: 'desc',
-        q: query
-      }
-    })
+      const response = await api.get('transactions', {
+        params: {
+          _sort: 'createdAt',
+          _order: 'desc',
+          q: query
+        },
+      })
 
     setTransactions(response.data)
-  }
+  } , [])
 
-  async function createTransaction(data: CreateTransactionInput) {
+
+
+  const createTransaction = useCallback(async (data: CreateTransactionInput) => {
 
     const { description, price, category, type } = data;
 
@@ -60,7 +62,9 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     })
 
     setTransactions(state => [response.data, ...state])
-  }
+    },
+    [],
+  )
 
   useEffect(() => {
     fetchTransactions()
